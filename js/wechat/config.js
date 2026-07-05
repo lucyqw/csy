2ba1a87d2dc55d79cf34781166aa1ef159a5cb98
lucyqw <syx2006118918@qq.com>
2026-07-05 22:04:56 +0800

    Delete js directory

--- js/wechat/config.js
+++
@@ -1,8 +0,0 @@
-/* 模块: js/config.js */
-
-// [VariableDeclaration] Variables: DB_NAME, DB_VERSION, STORE_NAME
-const DB_NAME = 'WeChatDB', DB_VERSION = 1, STORE_NAME = 'userProfile';
-
-// [VariableDeclaration] Variables: db
-let db = null;
-
